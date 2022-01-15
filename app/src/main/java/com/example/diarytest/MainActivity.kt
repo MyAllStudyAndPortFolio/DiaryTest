cb36ba478eb45222cc66ee16188186d2c77dd3bd
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-15 23:03:36 +0900

    temp push

--- app/src/main/java/com/example/diarytest/MainActivity.kt
+++ app/src/main/java/com/example/diarytest/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.diarytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply{
+                minValue=0
+                maxValue=9
+            }
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply{
+                minValue=0
+                maxValue=9
+            }
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply{
+                minValue=0
+                maxValue=9
+            }
+    }
+
 }
