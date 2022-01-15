864577992a121ea58fda04cb76848e8fad65b043
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-15 23:46:13 +0900

    making password and changing ui

--- app/src/main/java/com/example/diarytest/MainActivity.kt
+++ app/src/main/java/com/example/diarytest/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.example.diarytest
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.NumberPicker
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            passwordPreferences.getString("password","000")
+        }
     }
+
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
     private val numberPicker1: NumberPicker by lazy {
         findViewById<NumberPicker>(R.id.numberPicker1)
             .apply{
